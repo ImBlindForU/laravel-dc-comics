8cb63b143e80449ceb51bfcc905ee09628010970
ImBlindForU <samuele.zanchi10@gmail.com>
2022-12-21 15:49:23 +0100

    first commit

---
+++ database/migrations/2022_12_21_142626_create_fumetti_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fumetti', function (Blueprint $table) {
+            $table->id();
+            $table->string('tittle',100);
+            $table->text('description');
+            $table->text('thumb');
+            $table->string('price',20);
+            $table->string('series', 30);
+            $table->date('sale_date');
+            $table->string('tittle',100);
+            $table->string('type',40);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fumetti');
+    }
+};
